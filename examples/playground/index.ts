acc40922db0ced86f41444ee51724310115b25c9
KATT <alexander@n1s.se>
2020-09-17 11:10:13 +0200

    ✨ Add ts-node example

---
+++ examples/playground/index.ts
@@ -0,0 +1,18 @@
+import { cleanEnv, port, str } from '../../';
+
+const env = cleanEnv({
+  NODE_ENV: str({
+    choices: ['development', 'test', 'production'],
+  }),
+  MY_VAR: str({
+    devDefault: 'default value'
+  }),
+  PORT: port({
+    devDefault: 3000,
+  }),
+});
+
+
+console.log('PORT', env.PORT);
+
+console.log('All variables', env);
