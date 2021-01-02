d0f91a994112fe6495997d18cad1344f384c598c
Adam <adam.lj.haglund@gmail.com>
2021-01-02 22:52:25 +0100

    add default behavior test

--- test/treatEmptyAsNull.test.ts
+++ test/treatEmptyAsNull.test.ts
@@ -25,28 +25,36 @@ test('treatEmptyAsNull', () => {
     },
   );
 
-  expect(JSON.stringify(env)).toMatchInlineSnapshot(
-    `"{\\"defaulted\\":\\"devDefault\\",\\"alsoDefaulted\\":\\"devDefault\\",\\"notDefaulted\\":\\"notDefault\\"}"`,
-  );
-
   expect(env.defaulted).toBe('devDefault');
   expect(env.alsoDefaulted).toBe('devDefault');
   expect(env.notDefaulted).toBe('notDefault');
 });
 
-// test('dont default over empty strings (default behavior)', () => {
-//   const env = envsafe(
-//     {
-//       foo: str(),
-//     },
-//     {
-//       env: {
-//         foo: 'bar',
-//       },
-//     },
-//   );
-//
-//   expect(env.foo).toBe('bar');
-//   expect(() => (env as any).boo).not.toThrow();
-//   expect((env as any).boo).toBeUndefined();
-// });
+test('dont default over empty strings (default behavior)', () => {
+  const env = envsafe(
+    {
+      defaulted: str({
+        default: 'default',
+        devDefault: 'devDefault',
+      }),
+      empty: str({
+        default: 'default',
+        devDefault: 'devDefault',
+      }),
+      notDefaulted: str({
+        default: 'default',
+        devDefault: 'devDefault',
+      }),
+    },
+    {
+      env: {
+        empty: '',
+        notDefaulted: 'notDefault',
+      },
+    },
+  );
+
+  expect(env.empty).toBe('');
+  expect(env.defaulted).toBe('devDefault');
+  expect(env.notDefaulted).toBe('notDefault');
+});
